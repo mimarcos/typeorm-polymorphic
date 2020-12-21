d7a1a1f3934af2c3f4de8821042b64f3334ac5e1
Michael Marcos <mike@bravegamer.com>
2020-12-20 21:17:17 -0500

    Adding barrel export

---
+++ index.ts
@@ -0,0 +1,5 @@
+export * from './polymorphic/decorators';
+export * from './polymorphic/constants';
+export * from './polymorphic/polymorphic.interface';
+export * from './polymorphic/polymorphic.repository';
+export * from './polymorphic/repository.token.exception';
